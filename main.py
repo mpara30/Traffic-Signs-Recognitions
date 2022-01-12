c3446cd23e32e380932198ee95e2a20820f92cff
Mihai Para <para.mihai99@gmail.com>
2022-01-12 23:35:33 +0200

    Building CNN model

--- main.py
+++ main.py
@@ -37,3 +37,24 @@ print(X_t1.shape, X_t2.shape, y_t1.shape, y_t2.shape)
 
 y_t1 = to_categorical(y_t1, 43)
 y_t2 = to_categorical(y_t2, 43)
+
+# Model Build
+
+model = Sequential()
+
+model.add(Conv2D(filters=32, kernel_size=(5, 5), activation='relu'))
+model.add(Conv2D(filters=32, kernel_size=(5, 5), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.25))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.25))
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(rate=0.5))
+model.add(Dense(43, activation='softmax'))
+
+#Compilation
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
