8c80562fa87bed6b43c11324c0566614ec3bf3fd
Mihai Para <para.mihai99@gmail.com>
2022-01-13 18:56:15 +0200

    Minor fixes

--- gui.py
+++ gui.py
@@ -7,47 +7,47 @@ from keras.models import load_model
 
 model = load_model('traffic_classifier.h5')
 
-classes = {1: 'Speed limit (20km/h)',
-           2: 'Speed limit (30km/h)',
-           3: 'Speed limit (50km/h)',
-           4: 'Speed limit (60km/h)',
-           5: 'Speed limit (70km/h)',
-           6: 'Speed limit (80km/h)',
-           7: 'End of speed limit (80km/h)',
-           8: 'Speed limit (100km/h)',
-           9: 'Speed limit (120km/h)',
-           10: 'No passing',
-           11: 'No passing veh over 3.5 tons',
-           12: 'Right-of-way at intersection',
-           13: 'Priority road',
-           14: 'Yield',
+classes = {1: 'Limita de viteza (20km/h)',
+           2: 'Limita de viteza (30km/h)',
+           3: 'Limita de viteza (50km/h)',
+           4: 'Limita de viteza (60km/h)',
+           5: 'Limita de viteza (70km/h)',
+           6: 'Limita de viteza (80km/h)',
+           7: 'Sfarsitul limitei de viteza (80km/h)',
+           8: 'Limita de viteza (100km/h)',
+           9: 'Limita de viteza (120km/h)',
+           10: 'Nu treceti',
+           11: 'Nu trec vehicule cu greutate mai mare de 3.5 tone',
+           12: 'Drept de trecere la intersectie',
+           13: 'Drum cu prioritate',
+           14: 'Acordati prioritate',
            15: 'Stop',
-           16: 'No vehicles',
-           17: 'Veh > 3.5 tons prohibited',
-           18: 'No entry',
-           19: 'General caution',
-           20: 'Dangerous curve left',
-           21: 'Dangerous curve right',
-           22: 'Double curve',
-           23: 'Bumpy road',
-           24: 'Slippery road',
-           25: 'Road narrows on the right',
-           26: 'Road work',
-           27: 'Traffic signals',
-           28: 'Pedestrians',
-           29: 'Children crossing',
-           30: 'Bicycles crossing',
-           31: 'Beware of ice/snow',
-           32: 'Wild animals crossing',
+           16: 'Fara vehicule',
+           17: 'Vehicule cu greutate mai mare de 3.5 tone interzise',
+           18: 'Nu se intra',
+           19: 'Atentie generala',
+           20: 'Curba periculoasa la stanga',
+           21: 'Curba periculoasa la dreapta',
+           22: 'Curba dubla',
+           23: 'Drum accidentat',
+           24: 'Drum alunecos',
+           25: 'Drumul se ingusteaza pe dreapta',
+           26: 'Drum in lucru',
+           27: 'Semafor',
+           28: 'Pietoni',
+           29: 'Trec copii',
+           30: 'Trec biciclete',
+           31: 'Atentie la gheata/zapada',
+           32: 'Trec animale salbatice',
            33: 'End speed + passing limits',
-           34: 'Turn right ahead',
-           35: 'Turn left ahead',
-           36: 'Ahead only',
-           37: 'Go straight or right',
-           38: 'Go straight or left',
-           39: 'Keep right',
-           40: 'Keep left',
-           41: 'Roundabout mandatory',
+           34: 'Virati la dreapta inainte',
+           35: 'Virati la stanga inainte',
+           36: 'Inainte',
+           37: 'Mergeti drept sau la dreapta',
+           38: 'Mergeti drept sau la stanga',
+           39: 'Tineti dreapta',
+           40: 'Tineti stanga',
+           41: 'Sens giratoriu',
            42: 'End of no passing',
            43: 'End no passing vehicle with a weight greater than 3.5 tons'}
 
@@ -55,7 +55,7 @@ classes = {1: 'Speed limit (20km/h)',
 
 top = tk.Tk()
 top.geometry('800x600')
-top.title('Traffic sign classification')
+top.title('Clasificarea semnelor de circulatie')
 top.configure(background='#CDCDCD')
 label = Label(top, background='#CDCDCD', font=('arial', 15, 'bold'))
 sign_image = Label(top)
@@ -74,7 +74,7 @@ def classify(file_path):
 
 
 def show_classify_button(file_path):
-    classify_b = Button(top, text='Classify Image', command=lambda: classify(file_path), padx=10, pady=5)
+    classify_b = Button(top, text='Clasificati imaginea', command=lambda: classify(file_path), padx=10, pady=5)
     classify_b.configure(background='#364156', foreground='white', font=('arial', 10, 'bold'))
     classify_b.place(relx=0.79, rely=0.46)
 
@@ -93,12 +93,12 @@ def upload_image():
         pass
 
 
-upload = Button(top, text="Upload an image", command=upload_image, padx=10, pady=5)
+upload = Button(top, text="Incarcati o imagine", command=upload_image, padx=10, pady=5)
 upload.configure(background='#364156', foreground='white', font=('arial', 10, 'bold'))
 upload.pack(side=BOTTOM, pady=50)
 sign_image.pack(side=BOTTOM, expand=True)
 label.pack(side=BOTTOM, expand=True)
-heading = Label(top, text='Check Traffic Sign', pady=20, font=('arial', 20, 'bold'))
+heading = Label(top, text='Verificati semnul de circulatie', pady=20, font=('arial', 20, 'bold'))
 heading.configure(background='#CDCDCD', foreground='#364156')
 
 heading.pack()
